120701e63dabd69838ad9cbbd2ecdc6c94dddec0
Елена <110937221+velenavl@users.noreply.github.com>
2023-10-09 19:42:30 +0300

    Create BaseApp.py

---
+++ DZ4/BaseApp.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage:
+
+    def __init__(self, driver) -> None:
+        self.driver = driver
+        self.base_url = "https://test-stand.gb.ru/"
+
+    def find_element(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+                                                      message=f'Cant find element by locator{locator}')
+
+    def get_element_property(self, locator, property):
+        element = self.find_element(locator)
+        return element.value_of_css_property(property)
+
+    def go_to_site(self):
+        return self.driver.get(self.base_url)
+
+    def alert(self):
+        alert = self.driver.switch_to.alert
+        return alert.text
